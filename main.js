294019ada770bd2bedb648ff87596b062e200e53
Kuv1n <kevinszigetvari40@gmail.com>
2022-05-15 20:59:01 +0200

    new stuff

---
+++ main.js
@@ -0,0 +1,59 @@
+function delay(n) {
+    n = n || 2000;
+    return new Promise((done) => {
+        setTimeout(() => {
+            done();
+        }, n);
+    });
+}
+
+function pageTransition() {
+    var tl = gsap.timeline();
+    tl.to(".loading-screen", {
+        duration: 0.7,
+        width: "100%",
+        left: "0%",
+        ease: "Expo.easeInOut",
+    });
+
+    tl.to(".loading-screen", {
+        duration: 1,
+        width: "100%",
+        left: "100%",
+        ease: "Expo.easeInOut",
+        delay: .001,
+    });
+    tl.set(".loading-screen", { left: "-100%" });
+}
+
+function contentAnimation() {
+    return
+    // var tl = gsap.timeline();
+    // tl.from(".title-animate", { duration: 0.2, y: 40, opacity: -1, stagger: 0.100, delay: 0.4 });
+}
+
+$(function () {
+    barba.init({
+        sync: true,
+
+        transitions: [
+            {
+                async leave(data) {
+                    const done = this.async();
+
+                    pageTransition();
+                    await delay(1000);
+                    done();
+                },
+
+                async enter(data) {
+                    contentAnimation();
+                },
+
+                async once(data) {
+                    contentAnimation();
+                },
+            },
+        ],
+    });
+});
